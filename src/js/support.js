28a5b94ccbc0872e961257063495ba4a7c5b2e60
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2023-11-11 14:39:12 +0100

    h

--- src/js/support.js
+++ src/js/support.js
@@ -5,52 +5,52 @@ const foundations = [
         title: 'Save the Children',
         url:
             'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-        img: null,
+        img: "../img/supports logo/support_9.png",
     },
     {
         title: 'Project HOPE',
         url: 'https://www.projecthope.org/country/ukraine/',
-        img: null,
+        img: "../img/supports logo/support_1.png",
     },
     {
         title: 'UNITED24',
         url: 'https://u24.gov.ua/uk',
-        img: null,
+        img: "../img/supports logo/support_2.png",
     },
     {
         title: 'International Medical Corps',
         url: 'https://internationalmedicalcorps.org/country/ukraine/',
-        img: null,
+        img: "../img/supports logo/support_3.png",
     },
     {
         title: 'Medicins Sans Frontieres',
         url: 'https://www.msf.org/ukraine',
-        img: null,
+        img: "../img/supports logo/support_4.png",
     },
     {
         title: 'RAZOM',
         url: 'https://www.razomforukraine.org/',
-        img: null,
+        img: "../img/supports logo/support_5.png",
     },
     {
         title: 'Action against hunger',
         url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-        img: null,
+        img: "../img/supports logo/support_6.png",
     },
     {
         title: 'World vision',
         url: 'https://www.wvi.org/emergencies/ukraine',
-        img: null,
+        img: "../img/supports logo/support_7.png",
     },
     {
         title: 'Serhiy Prytula Charity Foundation',
         url: 'https://prytulafoundation.org/en',
-        img: null,
+        img: "../img/supports logo/support_8.png"
     },
 ];
 const Murkup = foundations.map((elem, index, arr) => ` <li class="support-foundation">
       <span>0${index + 1}</span>
-     <a href="${elem.url}"> <img src="${elem.img}" alt="${elem.title}"></a>
+     <a href="${elem.url}"> <img class="foundation-logo" src="${elem.img}" alt="${elem.title}"></a>
     </li>`).join("");
 foundationList.insertAdjacentHTML("afterbegin", Murkup);
 
@@ -58,22 +58,21 @@ const supportBtn = document.querySelector(".support-slider-button");
 supportBtn.addEventListener('click', handlerSupport);
 let up = false;
 function handlerSupport() {
-
     if (!up) {
-     foundationList.scrollBy(0, foundationList.querySelector("li").offsetHeight); 
-         if (foundationList.scrollTop === foundationList.offsetHeight - 40) {
-             up = true;
+        foundationList.scrollBy(0, 60); 
+        console.log("scroltop" + foundationList.scrollTop);
+        console.log(foundationList.offsetHeight);
+        if (foundationList.scrollTop >200) {
+            up = true;
+            return;
         }
     }
-
-   
   
     if (up) {
-        foundationList.scrollBy(0, -foundationList.querySelector("li").offsetHeight);
+        foundationList.scrollBy(0, -60);
         if (foundationList.scrollTop === 0) {
-              up = false; 
+            up = false; 
+            return;
     }
   }
-
-
 }
