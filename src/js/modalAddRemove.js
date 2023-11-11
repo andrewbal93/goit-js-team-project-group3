5722e8ba873aaa88c882fd4bddc8c5d0eaa7048f
Oleksandr Bry <134804799+Prybluda@users.noreply.github.com>
2023-11-11 14:25:05 +0100

    1267

--- src/js/modalAddRemove.js
+++ src/js/modalAddRemove.js
@@ -1,86 +1,69 @@
-import fetchBookById from './request';
+
+// import{fetchBookById, fetchBooksCategory, fetchAllTopBooks, fetchBooksBySelectedCategory} from './request';
 
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    const modal = document.getElementById('modal');
-    const closeIcon = document.getElementById('closeIcon');
-    const shoppingListBtn = document.getElementById('shoppingListBtn');
+    const openModalLink = document.querySelector('.open-modal');
+    const modal = document.querySelector('.modal');
+    const closeModalButton = modal.querySelector('.modal-close');
+    const addToShoppingListButton = modal.querySelector('.add-to-list');
     const backdrop = modal.querySelector('.modal-body');
 
-    closeIcon.addEventListener('click', closeModal);
-    backdrop.addEventListener('click', handleBackdropClick);
-    document.addEventListener('keydown', handleKeyDown);
-    shoppingListBtn.addEventListener('click', updateShoppingList);
+    // Open modal
+    openModalLink.addEventListener('click', function () {
+        modal.classList.add('open');
+        document.body.style.overflow = 'hidden'; // Заборона прокрутки фону
+    });
 
-    function updateShoppingList() {
-        // Оновлення інформації про список в localStorage
-        console.log('Shopping list updated');
-    }
-
-    function closeModal() {
+    // Close modal
+    const closeModal = function () {
         modal.classList.remove('open');
-        document.body.style.overflow = 'auto';
-        removeEventListeners();
-    }
+        document.body.style.overflow = ''; // Відновлення прокрутки фону
+    };
 
-    function handleBackdropClick(event) {
-        if (event.target === backdrop) {
-            closeModal();
-        }
-    }
+    closeModalButton.addEventListener('click', closeModal);
+    backdrop.addEventListener('click', closeModal);
 
-    function handleKeyDown(event) {
+    document.addEventListener('keydown', function (event) {
         if (event.key === 'Escape') {
             closeModal();
         }
+    });
+
+    // Add to / Remove from shopping list
+    shoppingListButton.addEventListener('click', function () {
+        // Оновлюємо інформацію про список в localStorage
+        let shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
+        const bookTitle = modal.querySelector('.book-title').textContent;
+
+        if (shoppingList.includes(bookTitle)) {
+            shoppingList = shoppingList.filter(item => item !== bookTitle);
+        } else {
+            shoppingList.push(bookTitle);
+        }
+
+        localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
+
+        // Оновлюємо текст кнопки в модалці
+        updateShoppingListButton(shoppingList);
+    });
+
+    // Функція для оновлення тексту кнопки відповідно до стану списку
+    function updateShoppingListButton(shoppingList) {
+        const bookTitle = modal.querySelector('.book-title').textContent;
+        const isBookInList = shoppingList.includes(bookTitle);
+
+        if (isBookInList) {
+            shoppingListButton.textContent = 'Remove from the shopping list';
+        } else {
+            shoppingListButton.textContent = 'Add to shopping list';
+        }
     }
 
-    function removeEventListeners() {
-        closeIcon.removeEventListener('click', closeModal);
-        backdrop.removeEventListener('click', handleBackdropClick);
-        document.removeEventListener('keydown', handleKeyDown);
-        shoppingListBtn.removeEventListener('click', updateShoppingList);
-    }
+    // Ініціалізація тексту кнопки при завантаженні сторінки
+    const initialShoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
+    updateShoppingListButton(initialShoppingList);
 });
 
 
-
-
-async function fetchBookDetails(bookId) {
-    try {
-        // Зробіть запит до API з використанням Axios
-        const response = await axios.get(`https://books-backend.p.goit.global/books/${bookId}`);
-
-        // Отримайте детальну інформацію про книгу з відповіді
-        const bookDetails = response.data;
-
-        // Виведіть інформацію про книгу (змініть console.log за потреби)
-        console.log('Book Image:', bookDetails.book_image);
-        console.log('Title:', bookDetails.title);
-        console.log('Author:', bookDetails.author);
-        console.log('Description:', bookDetails.description);
-        console.log('Buy Links:');
-        bookDetails.buy_links.forEach(link => {
-            console.log(`${link.name}: ${link.url}`);
-        });
-
-        // Поверніть об'єкт з детальною інформацією про книгу
-        return bookDetails;
-    } catch (error) {
-        // Обробка помилок
-        console.error('Error fetching book details:', error.message);
-        throw error; // Прокидування помилки для подальшої обробки
-    }
-}
-
-// Викликати функцію та передати ідентифікатор книги
-fetchBookDetails('your_book_id_here')
-    .then(bookDetails => {
-        // Робіть що завгодно з отриманою інформацією про книгу
-        console.log('Book Details:', bookDetails);
-    })
-    .catch(error => {
-        // Обробити помилки, якщо є
-        console.error('Error:', error.message);
-    });
---
+++ src/main.js
@@ -0,0 +1,3 @@
+// import refs from './js/modalAddRemove.js';
+// const {modal} = refs;
+// console.log(modal)
