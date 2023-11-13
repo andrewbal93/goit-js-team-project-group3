5b0db509164a36f86bd24bd07033d6236852677e
andrewbal93 <131342609+andrewbal93@users.noreply.github.com>
2023-11-14 00:34:04 +0200

    1

--- src/js/support.js
+++ src/js/support.js
@@ -5,47 +5,47 @@ const foundations = [
         title: 'Save the Children',
         url:
             'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-        img: "../img/supportsLogo/support_9.png",
+        img: "./img/supportsLogo/support_9.png",
     },
     {
         title: 'Project HOPE',
         url: 'https://www.projecthope.org/country/ukraine/',
-        img: "../img/supportsLogo/support_1.png",
+        img: "./img/supportsLogo/support_1.png",
     },
     {
         title: 'UNITED24',
         url: 'https://u24.gov.ua/uk',
-        img: "../img/supportsLogo/support_2.png",
+        img: "./img/supportsLogo/support_2.png",
     },
     {
         title: 'International Medical Corps',
         url: 'https://internationalmedicalcorps.org/country/ukraine/',
-        img: "../img/supportsLogo/support_3.png",
+        img: "./img/supportsLogo/support_3.png",
     },
     {
         title: 'Medicins Sans Frontieres',
         url: 'https://www.msf.org/ukraine',
-        img: "../img/supportsLogo/support_4.png",
+        img: "./img/supportsLogo/support_4.png",
     },
     {
         title: 'RAZOM',
         url: 'https://www.razomforukraine.org/',
-        img: "../img/supportsLogo/support_5.png",
+        img: "./img/supportsLogo/support_5.png",
     },
     {
         title: 'Action against hunger',
         url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-        img: "../img/supportsLogo/support_6.png",
+        img: "./img/supportsLogo/support_6.png",
     },
     {
         title: 'World vision',
         url: 'https://www.wvi.org/emergencies/ukraine',
-        img: "../img/supportsLogo/support_7.png",
+        img: "./img/supportsLogo/support_7.png",
     },
     {
         title: 'Serhiy Prytula Charity Foundation',
         url: 'https://prytulafoundation.org/en',
-        img: "../img/supportsLogo/support_8.png"
+        img: "./img/supportsLogo/support_8.png"
     },
 ];
 const Murkup = foundations.map((elem, index, arr) => ` <li class="support-foundation">
